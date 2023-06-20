540966d147595e6cbd770b7eb0363a6c6cb2a56f
digvijaybind <binddigvijay123@gmail.com>
2023-06-20 17:58:13 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View} from "react-native";
+import {Router, Scene, Stack} from "react-native-router-flux";
+import Products from "./screens/Products";
+import ProductInfo from "./screens/ProductInfo";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View>hii there</View>
+    // <Router>
+    //   //{" "}
+    //   <Stack key="root">
+    //     // <Scene key="products" component={Products} hideNavBar />
+    //     // <Scene key="ProductInfo" component={ProductInfo} hideNavBar />
+    //     //{" "}
+    //   </Stack>
+
+    // </Router>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
